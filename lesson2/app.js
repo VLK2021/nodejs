571335b21762b40971a9fb2c5c411b0b7539fdac
vlk2022 <volodymyrkostiuk2021@gmail.com>
2024-04-03 23:24:50 +0300

    lesson2

--- lesson2/app.js
+++ lesson2/app.js
@@ -1,1 +1,31 @@
 const express = require('express');
+
+const app = express();
+
+
+app.get('/', (req, res) => {
+    // res.write('dasfsdf');
+    res.end('ALL RIGHT');
+    // res.json('from json');
+    // res.status(403).end('sdc');
+    // res.sendStatus(400);
+})
+
+app.get('/users', (req, res) => {
+    res.json([
+        {name: 'Volodya'},
+        {name: 'Valya'}
+    ])
+})
+
+
+
+
+
+app.listen(5000, (err) => {
+    if (err) {
+        console.log(err);
+    }
+
+    console.log('Server is started on 5000');
+})
