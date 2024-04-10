3043910cb23ec13f8eaa41e6ebf82c910e93d603
vlk2022 <volodymyrkostiuk2021@gmail.com>
2024-04-10 17:40:37 +0300

    sraka

--- lesson2/app.js
+++ lesson2/app.js
@@ -1,3 +1,59 @@
+// const express = require('express');
+// const expressHandlebars = require('express-handlebars');
+// const path = require('path');
+//
+// const app = express();
+//
+// app.use(express.urlencoded({extended: true}));
+// app.use(express.json());
+//
+//
+// app.use(express.static(path.join(process.cwd(), 'views')));
+//
+//
+// app.set('view engine', '.hbs');
+// app.engine('.hbs', expressHandlebars({
+//     defaultLayout: false
+// }));
+// app.set('view', path.join(process.cwd(), 'views'));
+//
+//
+//
+//
+// app.get('/', (req, res) => {
+//     // res.write('dasfsdf');
+//     res.end('ALL RIGHT');
+//     // res.json('from json');
+//     // res.status(403).end('sdc');
+//     // res.sendStatus(400);
+// })
+//
+// app.get('/users', (req, res) => {
+//     res.json([
+//         {name: 'Volodya'},
+//         {name: 'Valya'}
+//     ])
+// })
+//
+// app.post('/users', (req, res) => {
+//     console.log(req.body);
+//
+//     res.end('user created');
+// })
+//
+//
+//
+//
+//
+// app.listen(5000, (err) => {
+//     if (err) {
+//         console.log(err);
+//     }
+//
+//     console.log('Server is started on 5000');
+// })
+
+
 const express = require('express');
 const expressHandlebars = require('express-handlebars');
 const path = require('path');
@@ -7,43 +63,42 @@ const app = express();
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
-
 app.use(express.static(path.join(process.cwd(), 'views')));
 
 
-app.set('view engine', '.hbs');
 app.engine('.hbs', expressHandlebars({
     defaultLayout: false
 }));
+app.set('view engine', '.hbs');
 app.set('views', path.join(process.cwd(), 'views'));
-
-
 
 
 app.get('/', (req, res) => {
     // res.write('dasfsdf');
-    res.end('ALL RIGHT');
+    // res.end('ALL RIGHT');
     // res.json('from json');
     // res.status(403).end('sdc');
     // res.sendStatus(400);
-})
+    console.log('Received GET request for /');
+
+    res.render('main')
+});
 
 app.get('/users', (req, res) => {
+    console.log('Received GET request for /users');
+
     res.json([
         {name: 'Volodya'},
         {name: 'Valya'}
-    ])
-})
+    ]);
+});
 
 app.post('/users', (req, res) => {
+    console.log('Received POST request for /users');
+
     console.log(req.body);
-
     res.end('user created');
-})
-
-
-
-
+});
 
 app.listen(5000, (err) => {
     if (err) {
@@ -51,4 +106,4 @@ app.listen(5000, (err) => {
     }
 
     console.log('Server is started on 5000');
-})
+});
