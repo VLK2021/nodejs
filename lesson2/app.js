d02490129155354171bb9b5abbf7bfdd4fdc3eee
vlk2022 <volodymyrkostiuk2021@gmail.com>
2024-04-10 16:37:13 +0300

    lesson2

--- lesson2/app.js
+++ lesson2/app.js
@@ -1,9 +1,22 @@
 const express = require('express');
+const expressHandlebars = require('express-handlebars');
+const path = require('path');
 
 const app = express();
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+
+
+app.use(express.static(path.join(process.cwd(), 'views')));
+
+
+app.set('view engine', '.hbs');
+app.engine('.hbs', expressHandlebars({
+    defaultLayout: false
+}));
+app.set('views', path.join(process.cwd(), 'views'));
+
 
 
 
